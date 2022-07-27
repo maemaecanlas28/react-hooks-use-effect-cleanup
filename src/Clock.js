515dd112cf3bd30bfcb73d0b26f38fb05ea4805b
Flordeliza Mae Canlas <maemaeperlas@gmail.com>
2022-07-26 19:03:32 -0700

    done

--- src/Clock.js
+++ src/Clock.js
@@ -4,9 +4,14 @@ function Clock() {
   const [time, setTime] = useState(new Date());
 
   useEffect(() => {
-    setInterval(() => {
+    const timerID = setInterval(() => {
       setTime(new Date());
+      console.log("getting triggered");
     }, 1000);
+  
+    return (() => {
+      clearInterval(timerID);
+    })
   }, []);
 
   return <div>{time.toString()}</div>;
